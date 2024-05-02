468ce4705ea3ab12d33bf96c570001e811124300
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2024-05-02 20:32:51 +0500

    1

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,9 +1,39 @@
 <template>
-  <q-page class="flex flex-center q-pa-lg">
-    <h5>Todo</h5>
+  <q-page class="bg-grey-3 colunm">
+    <q-list>
+
+      <q-item
+      v-for="task in tasks" :key="task.title"
+      tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="color" val="teal" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{task.title}}</q-item-label>
+          <q-item-label caption>With description</q-item-label>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
+export default{
+  data() {
+    return{
+      tasks:[
+        {
+          title: 'Hello1',
+          done: false,
+        },
+        {
+          title: 'Hello2',
 
+          done: false,
+        }
+      ]
+    };
+  },
+};
 </script>
