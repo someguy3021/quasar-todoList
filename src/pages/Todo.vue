322724b54e7a3aea77b272d1d510b4ec5507494f
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2024-05-07 19:08:37 +0500

    11.3

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,39 +1,69 @@
 <template>
   <q-page class="bg-grey-3 colunm">
-    <q-list>
-
+    <q-list separator bordered>
       <q-item
-      v-for="task in tasks" :key="task.title"
-      tag="label" v-ripple>
+        @click="task.done = !task.done"
+        clickable
+        :class="{ 'done bg-green-2': task.done }"
+        v-for="(task,index) in tasks"
+        :key="task.title"
+        v-ripple
+      >
         <q-item-section avatar>
-          <q-checkbox v-model="color" val="teal" color="primary" />
+          <q-checkbox
+            class="no-pointer-events"
+            v-model="task.done"
+            val="teal"
+            color="primary"
+          />
         </q-item-section>
         <q-item-section>
-          <q-item-label>{{task.title}}</q-item-label>
+          <q-item-label>{{ task.title }}</q-item-label>
           <q-item-label caption>With description</q-item-label>
         </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn @click.stop="deleteTask(index)" dense flat round color="primary" icon="delete" />
+        </q-item-section>
       </q-item>
-
     </q-list>
   </q-page>
 </template>
 
 <script>
-export default{
+export default {
   data() {
-    return{
-      tasks:[
+    return {
+      tasks: [
         {
-          title: 'Hello1',
+          title: "Hello1",
+          done: true,
+        },
+        {
+          title: "Hello2",
+
           done: false,
         },
         {
-          title: 'Hello2',
+          title: "Hello3",
 
           done: false,
-        }
-      ]
+        },
+      ],
     };
   },
+  methods:{
+    deleteTask(index){
+      this.tasks.splice(index,1)
+    }
+  }
 };
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: gray;
+  }
+}
+</style>
